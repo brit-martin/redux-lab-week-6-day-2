a1a5348c548b3ebc158037143209daf90d527697
Brittany Larson <brittanylarson@Brittanys-Air.localdomain>
2024-01-09 14:40:59 -0700

    start of lab

---
+++ src/components/HackerNews/HackerNews.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import Card from '../shared/Card/Card.jsx';
+import Loading from '../shared/Loading/Loading.jsx';
+import 
+
+export default function HackerNews() {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const articleCards = articles.map((article) => <Card key={article.id} article={article} />);
+  return (
+    <div className="news-container">
+      <img className='logo' src="../../assets/hackerNews.jpeg" alt="" />
+      {loading ? <Loading /> : <div>{articleCards}</div>}
+    </div>
+  );
+}
---
+++ src/reducers/hackerNewsReducer.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+const initialState = {
+    loading: false,
+    articles: [],
+}
+
+const REQUEST_ARTICLES = 'REQUEST_ARTICLES'
+const PENDING = 'PENDING'
+
+export const requestArticles = async (dispatch) => {
+    dispatch({ type: PENDING })
+    const articles = await axios.get('/api/hacker-news')
+    dispatch({ type: REQUEST_ARTICLES, payload: articles })
+}
+
+export default function reducer(state = initialState, action){
+    switch(action.type){
+        case "PENDING":
+            return {...state, loading: true}
+        case "REQUEST_ARTICLES":
+            return {loading: false, articles: action.payload }
+        default:
+            return state;
+    }
+}
